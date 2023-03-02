2c48895a752b419ccc6ab23a28ff65f838391576
thuan <lapthuan0805@gmail.com>
2023-03-02 14:20:42 +0700

    update route

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -39,6 +39,21 @@ const Home = () => {
     return () => clearInterval(intervalId);
   }, []);
 
+  useEffect(() => {
+    axios.post('https://api-vuon-thong-minh.onrender.com/users/user-data', {
+      token: window.localStorage.getItem("token"),
+    })
+      .then((data) => {
+ 
+       if(data.data.data == "token expired"){
+        window.localStorage.clear();
+        window.localStorage.setItem("loggedIn", "false");
+        window.localStorage.getItem("loggedIn");
+ 
+       }
+      })
+  },[])
+
   return (
     <>
       <div className="home">
