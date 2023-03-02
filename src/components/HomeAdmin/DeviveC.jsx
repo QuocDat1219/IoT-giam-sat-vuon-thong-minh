70249c09806b2e6878ee8d2119bb0dfca2b53c6a
BaoxRosie1 <94978764+BaoxRosie1@users.noreply.github.com>
2023-03-02 13:42:51 +0700

    update admin home

--- src/components/HomeAdmin/DeviveC.jsx
+++ src/components/HomeAdmin/DeviveC.jsx
@@ -10,9 +10,10 @@ import UsbIcon from "@mui/icons-material/Usb";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./Css/DeviceC.css";
-
+import RingLoader from "react-spinners/RingLoader"
 export const DeviceC = (props) => {
-  const [checkConnect, setCheckConnect] = useState("");
+  const [checkConnect, setCheckConnect] = useState(null);
+  console.log(checkConnect);
   useEffect(() => {
     const fetchData = async () => {
       await axios
@@ -37,7 +38,7 @@ export const DeviceC = (props) => {
     return () => clearInterval(intervalId);
   }, []);
 
-  return (
+  return checkConnect != null  ?(
     <div style={{ width: "100%" }} className="DeviceC">
       <Card {...props}>
         <CardContent>
@@ -48,6 +49,41 @@ export const DeviceC = (props) => {
               </Typography>
               <Typography color="textPrimary" variant="h4">
                 {checkConnect}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Avatar
+                sx={{
+                  backgroundColor: "#32ff7e",
+                  height: 56,
+                  width: 56,
+                }}
+              >
+                <UsbIcon />
+              </Avatar>
+            </Grid>
+          </Grid>
+          <Box
+            sx={{
+              alignItems: "center",
+              display: "flex",
+              pt: 2,
+            }}
+          ></Box>
+        </CardContent>
+      </Card>
+    </div>
+  ):(
+    <div style={{ width: "100%" }} className="DeviceC">
+      <Card {...props}>
+        <CardContent>
+          <Grid container spacing={3} sx={{ justifyContent: "space-between" }}>
+            <Grid item>
+              <Typography color="textSecondary" gutterBottom variant="overline">
+                Thiết bị đang kết nối
+              </Typography>
+              <Typography color="textPrimary" variant="h4">
+              <RingLoader size="42px"/>
               </Typography>
             </Grid>
             <Grid item>
